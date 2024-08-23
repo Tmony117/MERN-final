8d5f95ac539d0d0cde66b512b5482c1ede5fa175
Tmony <66956192+Tmony117@users.noreply.github.com>
2024-08-23 02:31:12 +0000

    value change done

--- client/app/components/Manager/OrderSummary/index.js
+++ client/app/components/Manager/OrderSummary/index.js
@@ -16,11 +16,11 @@ const OrderSummary = props => {
       <h2>Order Summary</h2>
       <div className='d-flex align-items-center summary-item'>
         <p className='summary-label'>Subtotal</p>
-        <p className='summary-value ml-auto'>Ghana Cedis {order.total}</p>
+        <p className='summary-value ml-auto'>gh₵{order.total}</p>
       </div>
       <div className='d-flex align-items-center summary-item'>
         <p className='summary-label'>Est. Sales Tax</p>
-        <p className='summary-value ml-auto'>Ghana Cedis {order.totalTax}</p>
+        <p className='summary-value ml-auto'>gh₵{order.totalTax}</p>
       </div>
 
       {/* <div className='d-flex align-items-center summary-item'>
@@ -31,7 +31,7 @@ const OrderSummary = props => {
       <hr />
       <div className='d-flex align-items-center summary-item'>
         <p className='summary-label'>Total</p>
-        <p className='summary-value ml-auto'>Ghana Cedis {order.totalWithTax}</p>
+        <p className='summary-value ml-auto'>gh₵{order.totalWithTax}</p>
       </div>
     </Col>
   );
